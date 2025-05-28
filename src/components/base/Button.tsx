b0eb0a9d41b99ddc2a2d609cd4ccd9ff5a4fbeaf
Alex <119893721+LexMorrison@users.noreply.github.com>
2025-05-28 16:07:43 +0200

    fetured header-86c3dzkr7 (#6)
    * fetured header-86c3dzkr7
    
    * fix-uncommenting footer

---
+++ src/components/base/Button.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { FC, ReactNode } from "react";
+
+type ButtonProps = {
+  onClick?: () => void;
+  children: ReactNode;
+  className: string;
+};
+
+const Button: FC<ButtonProps> = ({ children, onClick, className }) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={` rounded-md bg-accentMain text-white font-bold uppercase ${
+        className ?? ""
+      }`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
