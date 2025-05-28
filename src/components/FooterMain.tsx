8f278b567669c66e50d784ad3be75aea03acd5ab
Alex <119893721+LexMorrison@users.noreply.github.com>
2025-05-28 23:14:47 +0200

    featured mobile footer view (#7)

--- src/components/FooterMain.tsx
+++ src/components/FooterMain.tsx
@@ -10,109 +10,89 @@ export default function FooterMain() {
   return (
     <footer className="bg-mint">
       <Container>
-        <div className="flex justify-between">
-          <div className="flex gap-[50px] mt-[61px]">
-            {/* Categories */}
-            <div>
-              <h3 className="text-[18px] font-bold leading-[0.88] mb-2">
-                Categories
-              </h3>
+        <div className="pt-[13px] ">
+          {/* join us */}
+          <div className="w-full flex flex-col gap-[18px] mb-[18px] ">
+            <h1 className="text-[18px] font-bold ">
+              Pssst... Want the good stuff first?
+            </h1>
+
+            <div className="flex h-10 w-full ">
+              <input
+                className="outline-none py-3 px-4 text-[14px] flex-grow border border-accentMain border-r-0 rounded-l-[6px]"
+                id="email"
+                type="email"
+                placeholder="email@gmail.com "
+              />
+              <button
+                className="text-white text-[18px] font-bold leading-[0.88] py-3 pl-[22px] pr-[27px] bg-accentMain rounded-r-[6px]"
+                type="button"
+              >
+                Join
+              </button>
+            </div>
+          </div>
+
+          {/* 4 coloums  */}
+          <ul className="flex flex-row  w-full mb-4">
+            {/* categories */}
+            <li className="mr-[4px] ">
               <ul>
                 <li>
-                  <p className="text-[14px] leading-[2.14]">Jewelry</p>
+                  <p className="text-[18px] font-bold ">Categories</p>
                 </li>
                 <li>
-                  <p className="text-[14px] leading-[2.14]">Knitted Items</p>
+                  <p className="text-[14px] leading-4">Jewelry</p>
                 </li>
                 <li>
-                  <p className="text-[14px] leading-[2.14]">Candles & Soaps</p>
+                  <p className="text-[14px] leading-4">Knitted Items</p>
                 </li>
                 <li>
-                  <p className="text-[14px] leading-[2.14]">Home Decor</p>
+                  <p className="text-[14px] leading-4">Candles</p>
                 </li>
                 <li>
-                  <p className="text-[14px] leading-[2.14]">Gift Sets</p>
+                  <p className="text-[14px] leading-4">Gift Sets</p>
                 </li>
               </ul>
-            </div>
-
-            {/* User Guide */}
-            <div>
-              <h3 className="text-[18px] font-bold leading-[0.88] mb-2">
-                User Guide
-              </h3>
-              <ul>
+            </li>
+            {/* social media */}
+            <li className="flex  flex-row gap-2 items-end mr-[12px]">
+              <InstagramSVG />
+              <FacebookSVG />
+            </li>
+            {/* logo */}
+            <li className="flex flex-col items-center group mr-auto hover:text-accentMain active:text-accentMainActive ">
+              <LogoSVG />
+              <h2 className="text-[11px] font-bold">Crafted</h2>
+              <h3 className="text-[7px]">-Treasures-</h3>
+            </li>
+            {/* information */}
+            <li>
+              <ul className="flex flex-col ">
                 <li>
-                  <p className="text-[14px] leading-[2.14]">My Account</p>
+                  <p className="text-[18px] font-bold ">Information</p>
                 </li>
                 <li>
-                  <p className="text-[14px] leading-[2.14]">Favorite</p>
+                  <p className="text-[14px] leading-4">About Us</p>
                 </li>
                 <li>
-                  <p className="text-[14px] leading-[2.14]">How to Buy</p>
+                  <p className="text-[14px] leading-4">Contact Us</p>
                 </li>
                 <li>
-                  <p className="text-[14px] leading-[2.14]">
-                    Shipping & Delivery
+                  <p className="text-[14px] leading-4">
+                    Shipping &amp;
+                    <br /> Returning
                   </p>
                 </li>
               </ul>
-            </div>
-          </div>
-
-          {/* Logo */}
-          <LogoSVG className="mt-10 mb-[87px]" />
-          <div className="flex flex-col">
-            <div className="flex flex-col mt-[61px]">
-              <label
-                className="text-[18px] font-bold leading-[0.88] mb-4"
-                htmlFor="email"
-              >
-                Pssst... Want the good stuff first?
-              </label>
-              <div className="flex h-10 w-[359px] mb-[26px]">
-                <input
-                  className="outline-none py-3 px-4 text-[14px] flex-grow border border-accentMain border-r-0 rounded-l-[6px]"
-                  id="email"
-                  type="email"
-                  placeholder="email@gmail.com "
-                />
-                <button
-                  className="text-white font-bold leading-[0.88] py-3 pl-[22px] pr-[27px] bg-accentMain rounded-r-[6px]"
-                  type="button"
-                >
-                  Join
-                </button>
-              </div>
-
-              <ul className="flex gap-4">
-                <li>
-                  <a href="#">
-                    <InstagramSVG />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <FacebookSVG />
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            {/* Product Policy */}
-            <ul className="mt-auto flex gap-16">
-              <li>
-                <p className="text-[14px] text-lightGray leading-[2.14]">
-                  Product Policy
-                </p>
-              </li>
-              <li>
-                <p className="text-[14px] text-lightGray leading-[2.14]">
-                  © 2025 Shop. All Rights Reserved.
-                </p>
-              </li>
-            </ul>
-          </div>
+            </li>
+          </ul>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-[10px] text-lightGray">Product Policy</p>
+          <p className="text-[10px] text-lightGray">
+            &copy; 2025 Shop. All Rights Reserved.
+          </p>
         </div>
       </Container>
     </footer>
